da8bd3cbf47507c357d38fd8923e394aad2292c7
Stefan Arentz <stefan@arentz.ca>
2021-05-29 09:09:20 -0400

    Now building both armhf and arm64

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 set -x
 
-env GOOS=linux GOARCH=arm GOARM=5 go build && nfpm pkg --packager deb
+rm -f mijia-hub
+env GOOS=linux GOARCH=arm GOARM=5 go build \
+  && env NFPM_ARCH=armhf nfpm pkg --packager deb
+
+rm -f mijia-hub
+env GOOS=linux GOARCH=arm64 go build \
+  && env NFPM_ARCH=arm64 nfpm pkg --packager deb
 
